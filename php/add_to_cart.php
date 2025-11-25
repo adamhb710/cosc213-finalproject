e73bbf1f3c5697ce9be416e51cbcf8581aab6aa1
Shire-Bennett <stefanwells123@gmail.com>
2025-11-24 21:25:59 -0800

    Update add_to_cart.php

--- php/add_to_cart.php
+++ php/add_to_cart.php
@@ -3,9 +3,26 @@
 session_start();
 require_once 'config.php';
 
+// checking the incoming POST data
 if (!isset($_POST['product_id']) || !isset($_POST['quantity'])) {
     $_SESSION['message'] = "Missing product information.";
-    header("Location: add_to_cart.php");
+    header("Location: cart.php");
+    exit();
+}
+
+
+$product_id = $_POST['product_id'];
+$quantity = $_POST['quantity'];
+
+if ($product_id <= 0) {
+    $_SESSION['message'] = "Invalid product ID.";
+    header("Location: cart.php");
+    exit();
+}
+
+if ($quantity <= 0) {
+    $_SESSION['message'] = "Invalid quantity.";
+    header("Location: cart.php");
     exit();
 }
 ?>
