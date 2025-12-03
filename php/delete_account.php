4e2a450931dd9840956555a05c1939dcd00f7476
adamhb710 <adamhb.710@gmail.com>
2025-12-02 19:27:28 -0800

    Added functionality for deleting accounts in delete_account.php

--- php/delete_account.php
+++ php/delete_account.php
@@ -1,3 +1,13 @@
 <?php
-//insert delete user code here
+session_start();
+require_once 'config.php';
+
+$user = $_POST['user_id'];
+
+$stmt = $conn->prepare("DELETE FROM users WHERE id = " . $user . ";");
+$stmt->execute();
+
+$_SESSION['message'] = "Account successfully deleted.";
+header("Location: logout.php");
+exit();
 ?>
